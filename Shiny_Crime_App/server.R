35343b9a1c4fc3d944042635da74bb17eed278fb
sedm2610 <blaequayle@hotmail.com>
2017-07-04 17:01:35 +0100

    First load of Shiny App

---
+++ Shiny_Crime_App/server.R
@@ -0,0 +1,83 @@
+shinyServer(function(input, output) {
+  library(dplyr)
+  library(ggmap)
+  library(lubridate)
+  library(leaflet)
+  library(sp)
+  library(rgdal)
+  library(maptools)
+  library(KernSmooth)
+  
+  latlon <- reactive({
+    filter(bike16, bike16$date>=input$dateRange[1]&bike16$date<=input$dateRange[2])
+  })
+  
+  output$popmap <- renderLeaflet({
+    leaflet(bike16) %>%
+      addTiles(group = "OSM (default)") %>%
+      addProviderTiles(providers$Stamen.Toner, group = "Toner") %>%
+      addProviderTiles(providers$Stamen.TonerLite, group = "Toner Lite") %>%
+      fitBounds(~min(longitude), ~min(latitude), ~max(longitude), ~max(latitude)) %>%
+      addLayersControl(
+        baseGroups = c("OSM (default)", "Toner", "Toner Lite"),
+        options = layersControlOptions(collapsed = FALSE)
+      )
+  })
+  
+  observe({
+    leafletProxy("popmap", data = latlon()) %>%
+      clearShapes() %>%
+      clearControls() %>%
+      addCircles(~longitude,~latitude,color="blue")
+  })
+  
+
+
+  
+  output$heatmap <- renderLeaflet({
+    latlong <- select(bike16, longitude,latitude)
+    latlong <- filter(latlong, bike16$date>=input$dateRange1[1]&bike16$date<=input$dateRange1[2])
+    kde <- bkde2D(latlong,
+                  bandwidth=c(.0045, .0068), gridsize = c(75,75))
+    CL <- contourLines(kde$x1 , kde$x2 , kde$fhat)
+    
+    ## EXTRACT CONTOUR LINE LEVELS
+    LEVS <- as.factor(sapply(CL, `[[`, "level"))
+    NLEV <- length(levels(LEVS))
+    
+    ## CONVERT CONTOUR LINES TO POLYGONS
+    pgons <- lapply(1:length(CL), function(i)
+      Polygons(list(Polygon(cbind(CL[[i]]$x, CL[[i]]$y))), ID=i))
+    spgons = SpatialPolygons(pgons)
+    leaflet(spgons) %>% addTiles() %>%
+      addPolygons(color = heat.colors(NLEV, NULL)[LEVS]) %>%
+      addCircles(lng = latlong$longitude, lat = latlong$latitude,
+                 radius = .5, opacity = .2, col = "blue")
+  })
+  
+  output$safetymap <- renderLeaflet({
+    leaflet() %>%
+      setView(lng = -0.1288, lat = 51.5102, zoom=11) %>%
+      addTiles(options = providerTileOptions(noWrap = TRUE))
+  })
+  
+  ## Observe mouse clicks and add circles
+  observeEvent(input$safetymap_click, {
+    ## Get the click info like had been doing
+    click <- input$safetymap_click
+    clat <- click$lat
+    clng <- click$lng
+    #address <- revgeocode(c(clng,clat))
+    thefts <- nrow(filter(bike16, latitude>=(clat-0.0089)&latitude<=(clat+0.0089)&
+                    longitude>=(clng-0.0089/cos(clat*0.018))&longitude<=(clng+0.0089/cos(clat*0.018))))
+    leafletProxy("safetymap") %>% # use the proxy to save computation
+      addCircles(lng=clng, lat=clat, group='circles',
+                 weight=1, radius=dist, color='black', fillColor=ifelse(thefts<100,'green',ifelse(thefts>=100&thefts<500,'orange', ifelse(thefts>500, 'red'))),
+                 fillOpacity=0.5, opacity=1,
+                 popup = paste("No. of bicycle thefts: ", thefts))
+  })
+  
+  observeEvent(input$button,{
+    leafletProxy("safetymap")%>%clearShapes()
+  })
+})
---
+++ Shiny_Crime_App/ui.R
@@ -0,0 +1,53 @@
+library(shiny)
+library(leaflet)
+
+shinyUI(navbarPage(title=div(img(src="skyline.png", width="27%", align = "right"),"London Bicycle Theft Analysis"),
+                       tabPanel("Location of Bicycle Thefts",
+                            sidebarLayout(
+                              sidebarPanel(
+                                
+                                dateRangeInput('dateRange',
+                                               label = 'Filter records between dates:',
+                                               start = min(bike16$date), end = max(bike16$date) 
+                                ),
+                                width=3
+                              ),
+                              mainPanel(
+                                p("The plot below shows the thefts of bicycles in the 12 inner London boroughs for 2016.
+                                  The data has been obtained from the open access site http://data.police.uk.
+                                  The sidebar panel on the left allows the user to select a data range."),
+                                leafletOutput("popmap", width="100%", height=800)
+                              )
+                            )
+                   ),
+                   tabPanel("Bicycle Heat Map",
+                            sidebarLayout(
+                            sidebarPanel(
+                              dateRangeInput('dateRange1',
+                                             label = 'Filter records between dates:',
+                                             start = min(bike16$date), end = max(bike16$date)
+                              ),
+
+                              width=3
+                            ),
+                              mainPanel(
+                                p("This heatmap allows the user to identify hotspots for bicycle theft.
+                                  Specific date ranges can be selected from the sidebar panel to filter out records for a particular time period."),
+                                leafletOutput("heatmap", width="100%",height=800)
+                              )
+                            )     
+                   ),
+                   tabPanel("How Safe Is This?",
+                            mainPanel(
+                                h2("Click On Map to Get Safety Zone"),
+                                
+                                p("The app calculates the number of bicycle thefts that have taken place in a radius of 1 Km from 
+                                  the point that has been clicked on the map. If the number is less than 10, the circle is coloured green.
+                                  Between 10 to 50 thefts, the circle is rendered in orange colour and beyond 50, the colour of the circle is red.
+                                  The Met Police can encourage the users of this app to be aware of where they choose to lock their bikes."),
+                                actionButton("button","Clear Circles"),
+                                leafletOutput("safetymap", click = "safetymap_click", width = "100%",height=800)
+                              )
+                   )
+                   
+))
