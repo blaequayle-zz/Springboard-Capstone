067068fd74eadb108b57a46d7acba482a1aff02f
sedm2610 <blaequayle@hotmail.com>
2017-07-04 20:15:07 +0100

    Added global.R file to set up connection to RDS

--- Shiny_Crime_App/global.R
+++ Shiny_Crime_App/global.R
@@ -2,5 +2,15 @@ library(tidyverse)
 library(readr)
 library(shiny)
 library(utils)
+library(lubridate)
+library(KernSmooth)
+library(sp)
+library(rgdal)
+library(leaflet)
+library(htmltools)
+library(RColorBrewer)
 
-bike16 <- read.csv("bike16.csv")
+bike16 <- readRDS("bike16.rds")
+
+
+
